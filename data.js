0fed57de5caffd4c0de1bca8f71375a5428d669d
krquinton <nscrexgst.dotr@gmail.com>
2023-04-10 12:43:43 +0800

    initialcommit

---
+++ data.js
@@ -0,0 +1,732 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-artex-1",
+      "name": "Artex 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.0286409442547937,
+        "pitch": 1.5707963267948966,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04089387818972945,
+          "pitch": -0.2937853440805327,
+          "rotation": 0,
+          "target": "2-artex-3"
+        },
+        {
+          "yaw": -0.844186349685021,
+          "pitch": 1.0755618726355554,
+          "rotation": 0,
+          "target": "1-artex-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8721907792274557,
+          "pitch": 1.4232168449545561,
+          "title": "Artex",
+          "text": "Pumping Station"
+        }
+      ]
+    },
+    {
+      "id": "1-artex-2",
+      "name": "Artex 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.43095801476221496,
+        "pitch": 0.5007138304773129,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9994113863306016,
+          "pitch": 0.5999596921332415,
+          "rotation": 0,
+          "target": "2-artex-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.06898377559720359,
+          "pitch": 0.6395838896284545,
+          "title": "Artex",
+          "text": "Pumping Station"
+        }
+      ]
+    },
+    {
+      "id": "2-artex-3",
+      "name": "Artex 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.2497903043589531,
+        "pitch": 0.5005786067191167,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1902227235754541,
+          "pitch": 0.3613113914256907,
+          "rotation": 0,
+          "target": "0-artex-1"
+        },
+        {
+          "yaw": 0.32352712370231984,
+          "pitch": 0.7668450612649789,
+          "rotation": 0,
+          "target": "1-artex-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.06707997023449686,
+          "pitch": 0.5787550626209992,
+          "title": "Artex",
+          "text": "Pumping Station"
+        }
+      ]
+    },
+    {
+      "id": "3-merville-dampalit-1",
+      "name": "Merville Dampalit 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.07786853585821163,
+        "pitch": 0.28165152964349005,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5593227067515052,
+          "pitch": 0.13536626359975656,
+          "rotation": 0,
+          "target": "4-merville-dampalit-2"
+        },
+        {
+          "yaw": 0.09740146868333355,
+          "pitch": -0.4802239856877826,
+          "rotation": 0,
+          "target": "5-merville-dampalit-3"
+        },
+        {
+          "yaw": -0.18080261585313906,
+          "pitch": 0.3712287044361524,
+          "rotation": 0,
+          "target": "6-merville-dampalit-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.010266342747870638,
+          "pitch": 0.6405355475244274,
+          "title": "Merville Dampalit",
+          "text": "Pumping Station"
+        }
+      ]
+    },
+    {
+      "id": "4-merville-dampalit-2",
+      "name": "Merville Dampalit 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.259114265873011,
+        "pitch": 0.6295740074383982,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06718517164283355,
+          "pitch": 0.07743663139902779,
+          "rotation": 0,
+          "target": "3-merville-dampalit-1"
+        },
+        {
+          "yaw": 1.1413587583545386,
+          "pitch": 0.49796296623632585,
+          "rotation": 0,
+          "target": "6-merville-dampalit-4"
+        },
+        {
+          "yaw": 0.5670317031885954,
+          "pitch": -1.0600219671402424,
+          "rotation": 0,
+          "target": "5-merville-dampalit-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4187542654387748,
+          "pitch": 0.7457426704499177,
+          "title": "Merville Dampalit",
+          "text": "Pumping Station"
+        }
+      ]
+    },
+    {
+      "id": "5-merville-dampalit-3",
+      "name": "Merville Dampalit 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.823894588044963,
+        "pitch": 1.5707963267948966,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2170136376781215,
+          "pitch": 0.8855685278308449,
+          "rotation": 0,
+          "target": "3-merville-dampalit-1"
+        },
+        {
+          "yaw": 0.026945585205771394,
+          "pitch": 1.31878883079235,
+          "rotation": 0,
+          "target": "4-merville-dampalit-2"
+        },
+        {
+          "yaw": -2.9194923682481093,
+          "pitch": 1.2916855511610787,
+          "rotation": 0,
+          "target": "6-merville-dampalit-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0405776928786867,
+          "pitch": 1.4246497799697533,
+          "title": "Merville Dampalit",
+          "text": "Pumping Station"
+        }
+      ]
+    },
+    {
+      "id": "6-merville-dampalit-4",
+      "name": "Merville Dampalit 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.41619389855249445,
+        "pitch": 0.09756556255624105,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7713901665007379,
+          "pitch": -0.7555742925017306,
+          "rotation": 0,
+          "target": "3-merville-dampalit-1"
+        },
+        {
+          "yaw": 0.04406361211567322,
+          "pitch": -0.6009011791920642,
+          "rotation": 0,
+          "target": "4-merville-dampalit-2"
+        },
+        {
+          "yaw": 0.098817881744246,
+          "pitch": -1.4357343710966965,
+          "rotation": 0,
+          "target": "6-merville-dampalit-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.37509109245903716,
+          "pitch": 0.2916525284611744,
+          "title": "Merville Dampalit",
+          "text": "Pumping Station"
+        }
+      ]
+    },
+    {
+      "id": "7-balot-1",
+      "name": "Balot 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3021735367580742,
+        "pitch": 1.0885546504828607,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.006305786514733569,
+          "pitch": 0.7984527377480877,
+          "rotation": 0,
+          "target": "8-balot-2"
+        },
+        {
+          "yaw": 2.5898496951573673,
+          "pitch": 0.4484994455917839,
+          "rotation": 0,
+          "target": "9-balot-3"
+        },
+        {
+          "yaw": -3.132605588859997,
+          "pitch": 0.6091302304901447,
+          "rotation": 0,
+          "target": "10-balot-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-balot-2",
+      "name": "Balot 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.26734556165144063,
+        "pitch": 0.5741722873122068,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5809698244603982,
+          "pitch": -0.03307473885750767,
+          "rotation": 0,
+          "target": "9-balot-3"
+        },
+        {
+          "yaw": -3.0641985993745653,
+          "pitch": -0.4946033432395929,
+          "rotation": 0,
+          "target": "7-balot-1"
+        },
+        {
+          "yaw": -3.0885116545087286,
+          "pitch": 0.24512585520325736,
+          "rotation": 0,
+          "target": "10-balot-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-balot-3",
+      "name": "Balot 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.522565955318165,
+        "pitch": 0.34665392779532844,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9451878102054687,
+          "pitch": 0.1767673851240339,
+          "rotation": 0,
+          "target": "8-balot-2"
+        },
+        {
+          "yaw": 2.0842700190784917,
+          "pitch": -0.21734721488225617,
+          "rotation": 0,
+          "target": "7-balot-1"
+        },
+        {
+          "yaw": 1.828391043226782,
+          "pitch": 0.42348926216250504,
+          "rotation": 0,
+          "target": "10-balot-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-balot-4",
+      "name": "Balot 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.4994385282863423,
+        "pitch": 0.2853332489852338,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18644624256768694,
+          "pitch": -0.1338447720209004,
+          "rotation": 0,
+          "target": "9-balot-3"
+        },
+        {
+          "yaw": 0.9528708049306207,
+          "pitch": -0.5963502007344026,
+          "rotation": 0,
+          "target": "7-balot-1"
+        },
+        {
+          "yaw": -1.5910253729678434,
+          "pitch": -0.036466661642158726,
+          "rotation": 0,
+          "target": "8-balot-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-herrera-1",
+      "name": "Herrera 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.0043128057052737034,
+        "pitch": 0.25693845856281605,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.33158084496915663,
+          "pitch": -0.11472767834331421,
+          "rotation": 0,
+          "target": "12-herrera-2"
+        },
+        {
+          "yaw": 0.560386654077675,
+          "pitch": -1.0213888859887739,
+          "rotation": 0,
+          "target": "13-herrera-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.01665847676841281,
+          "pitch": 0.30671489717896705,
+          "title": "Herrera",
+          "text": "Pumping Station"
+        }
+      ]
+    },
+    {
+      "id": "12-herrera-2",
+      "name": "Herrera 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.37725963062338685,
+        "pitch": 0.7032083942732967,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.361133717398884,
+          "pitch": -0.07677445344869938,
+          "rotation": 0,
+          "target": "11-herrera-1"
+        },
+        {
+          "yaw": -1.0626977572108594,
+          "pitch": -0.921322179813421,
+          "rotation": 0,
+          "target": "13-herrera-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3481340232843433,
+          "pitch": 0.9518775836139959,
+          "title": "Herrera",
+          "text": "Pumping Station"
+        }
+      ]
+    },
+    {
+      "id": "13-herrera-3",
+      "name": "Herrera 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.7247503538680853,
+        "pitch": 1.5707963267948966,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8713592405937423,
+          "pitch": 1.3438142663455803,
+          "rotation": 0,
+          "target": "12-herrera-2"
+        },
+        {
+          "yaw": -1.2698900922331813,
+          "pitch": 0.8885806354266368,
+          "rotation": 0,
+          "target": "11-herrera-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Navotas-Malabon Pumping Stations",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
